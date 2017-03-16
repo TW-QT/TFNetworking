6b2558c849e44bc36d0f09cd5b076306e9080611
Donkey <taofei0610@gmail.com>
2017-03-16 17:34:22 +0800

    try to fix afn file not fount

--- TFNetworking/TFNetWorkingManager.h
+++ TFNetworking/TFNetWorkingManager.h
@@ -1,13 +1,13 @@
 //
 //  TFNetWorkingManager.h
-//  AFNRequestTool
+//  TFNetworking
 //
 //  Created by Donkey-Tao on 2016/5/12.
 //  Copyright © 2016年 http://taofei.me All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-
+#import <AFNetworking/AFNetworking.h>
 
 typedef void (^SuccessBlock)(id responseObject);
 typedef void (^FailedBlock)(NSError*error);
--- TFNetworking/TFNetWorkingManager.m
+++ TFNetworking/TFNetWorkingManager.m
@@ -1,13 +1,13 @@
 //
 //  TFNetWorkingManager.m
-//  AFNRequestTool
+//  TFNetworking
 //
 //  Created by Donkey-Tao on 2016/5/12.
 //  Copyright © 2016年 http://taofei.me All rights reserved.
 //
 
 #import "TFNetWorkingManager.h"
-#import <AFNetworking.h>
+
 
 @implementation TFNetWorkingManager
 /**
