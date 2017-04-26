f4f374a64e9b57fb7a39cc259c8f4e8393258c8b
Donkey-Tao <taofei0610@gmail.com>
2017-04-26 10:33:50 +0800

    solve error -999

--- TFNetworking/Session/TFHTTPSessionManager.h
+++ TFNetworking/Session/TFHTTPSessionManager.h
@@ -41,6 +41,14 @@ typedef void (^FailedBlock)(NSError*error);
 
 
 
+
+
+
+
+#pragma mark - 网络请求方法
+
+
+
 /**
  TFNetworking 网络请求（HTTPS）
  
@@ -51,6 +59,93 @@ typedef void (^FailedBlock)(NSError*error);
  @param failedBlock 网络请求失败的回调
  */
 + (void)tf_RequestURLString:(NSString *)URLString HttpMethod:(NSInteger)method  Parameters:(NSDictionary *)parameters  succeed:(SuccessBlock)successBlock failure:(FailedBlock)failedBlock;
+
+/**
+ * @brief iOS自带网络请求框架
+ * @param urlstring URL地址的字符串
+ * @param method URL地址的字符串
+ * @param params 请求中使用的参数
+ * @param SuccessBlock 请求成功的回调
+ * @param failedBlock 请求失败的回调
+ */
++ (void)requestURL:(NSString *)urlstring httpMethod:(NSInteger)method params:(NSMutableDictionary *)params complection:(SuccessBlock)SuccessBlock failed:(FailedBlock)failedBlock;
+
+/**
+ * @brief AFNetworking数据请求(HTTP)
+ * @param URLString URL地址的字符串
+ * @param method URL地址的字符串
+ * @param parameters 请求中使用的参数
+ * @param successBlock 请求成功的回调
+ * @param failedBlock 请求失败的回调
+ */
++(void)requestAFURL:(NSString *)URLString httpMethod:(NSInteger)method parameters:(id)parameters succeed:(SuccessBlock)successBlock failure:(FailedBlock)failedBlock;
+
+/**
+ * @brief AFNetworking网络请求(HTTPS)
+ * @param URLString URL地址的字符串
+ * @param method URL地址的字符串
+ * @param parameters 请求中使用的参数
+ * @param successBlock 请求成功的回调
+ * @param failedBlock 请求失败的回调
+ */
++ (void)requestAFURL:(NSString *)URLString httpMethod:(NSInteger)method Signature:(NSString *)signature Parameters:(NSDictionary *)parameters RequestTimes:(float)requestTimes succeed:(SuccessBlock)successBlock failure:(FailedBlock)failedBlock;
+
+
+
+#pragma mark - 上传图片
+
+/**
+ * @brief 上传单张图片
+ * @param URLString URL地址的字符串
+ * @param imageData URL地址的字符串
+ * @param parameters 请求中使用的参数
+ * @param successBlock 请求成功的回调
+ * @param failedBlock 请求失败的回调
+ */
++(void)requestAFURL:(NSString *)URLString parameters:(id)parameters imageData:(NSData *)imageData succeed:(SuccessBlock)successBlock failure:(FailedBlock)failedBlock;
+
+
+/**
+ * @brief 上传多张图片
+ * @param URLString URL地址的字符串
+ * @param parameters 请求中使用的参数
+ * @param imageDataArray URL地址的字符串
+ * @param successBlock 请求成功的回调
+ * @param failedBlock 请求失败的回调
+ */
++(void)requestAFURL:(NSString *)URLString parameters:(id)parameters  imageDataArray:(NSArray *)imageDataArray succeed:(SuccessBlock)successBlock failure:(FailedBlock)failedBlock;
+
+
+/**
+ * @brief 上传文件
+ * @param URLString URL地址的字符串
+ * @param parameters 请求中使用的参数
+ * @param fileData URL地址的字符串
+ * @param successBlock 请求成功的回调
+ * @param failedBlock 请求失败的回调
+ */
++(void)requestAFURL:(NSString *)URLString parameters:(id)parameters fileData:(NSData *)fileData succeed:(SuccessBlock)successBlock  failure:(FailedBlock)failedBlock;
+
+
+
+#pragma mark - 字符串与JSON的转换
+
+/**
+ * @brief 把格式化的JSON格式的字符串转换成字典
+ * @param jsonString JSON格式的字符串
+ * @return 返回字典
+ */
++ (NSDictionary *)dictionaryWithJsonString:(NSString *)jsonString;
+
+
+/**
+ * @brief 把字典转换成字符串
+ * @param paramDict 字典
+ * @param _type 类型
+ * @return 返回字符串
+ */
++(NSString*)URLEncryOrDecryString:(NSDictionary *)paramDict IsHead:(BOOL)_type;
+
 
 
 @end
